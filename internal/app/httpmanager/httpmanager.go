7e97a8a51554e9cd916d2e73a25f69034217d333
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2024-12-26 23:17:46 +0300

    shutdown ctx

--- internal/app/httpmanager/httpmanager.go
+++ internal/app/httpmanager/httpmanager.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/Mikhalevich/tg-bonus-points-bot/internal/app/httpmanager/handler"
+	"github.com/Mikhalevich/tg-bonus-points-bot/internal/infra/logger"
 )
 
 func Start(
@@ -28,13 +29,21 @@ func Start(
 		WriteTimeout: time.Second * 10,
 	}
 
-	//nolint:errcheck
-	go srv.ListenAndServe()
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			logger.FromContext(ctx).
+				WithError(err).
+				Error("service listen and serve error")
+		}
+	}()
 
 	<-ctx.Done()
 
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
 	//nolint:contextcheck
-	if err := srv.Shutdown(context.Background()); err != nil {
+	if err := srv.Shutdown(shutdownCtx); err != nil {
 		return fmt.Errorf("shutdown: %w", err)
 	}
 
