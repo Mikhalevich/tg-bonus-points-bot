aaf0838bd0c0461ee339fb7f7cfb28bf8094140e
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2024-12-29 22:57:33 +0300

    stub for cancel callback query

--- internal/app/tgbot/routes.go
+++ internal/app/tgbot/routes.go
@@ -6,11 +6,13 @@ import (
 
 	"github.com/Mikhalevich/tg-bonus-points-bot/internal/app/tgbot/router"
 	"github.com/Mikhalevich/tg-bonus-points-bot/internal/domain/port/msginfo"
+	"github.com/Mikhalevich/tg-bonus-points-bot/internal/domain/port/order"
 )
 
 type OrderProcessor interface {
 	MakeOrder(ctx context.Context, msgInfo msginfo.Info) error
 	GetActiveOrder(ctx context.Context, msgInfo msginfo.Info) error
+	CancelOrder(ctx context.Context, id order.ID) error
 }
 
 func Routes(o OrderProcessor) RouteRegisterFunc {
@@ -21,6 +23,7 @@ func Routes(o OrderProcessor) RouteRegisterFunc {
 		r.AddMenuCommand("/get_active_order", "retrieve active order", o.GetActiveOrder)
 
 		r.AddDefaultTextHandler(makeDefaultHandler(r))
+		r.AddDefaultCallbackQueryHander(makeDefaultCallbackQueryHandler(o))
 	}
 }
 
@@ -41,6 +44,21 @@ func makeStartHandler(r router.Register) msginfo.Handler {
 func makeDefaultHandler(r router.Register) msginfo.Handler {
 	return func(ctx context.Context, info msginfo.Info) error {
 		// skip message.
+		return nil
+	}
+}
+
+func makeDefaultCallbackQueryHandler(processor OrderProcessor) msginfo.Handler {
+	return func(ctx context.Context, info msginfo.Info) error {
+		orderID, err := order.IDFromString(info.Data)
+		if err != nil {
+			return fmt.Errorf("invalid order id: %w", err)
+		}
+
+		if err := processor.CancelOrder(ctx, orderID); err != nil {
+			return fmt.Errorf("cancel order: %w", err)
+		}
+
 		return nil
 	}
 }
