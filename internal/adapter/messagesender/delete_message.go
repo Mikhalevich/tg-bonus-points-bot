4c67d0c734eee041b7dec266ed79e8e4e7d991c5
Michael <michael.mikhalevich@gmail.com>
2025-01-09 22:25:10 +0300

    delete message (#29)

---
+++ internal/adapter/messagesender/delete_message.go
@@ -0,0 +1,25 @@
+package messagesender
+
+import (
+	"context"
+
+	"github.com/go-telegram/bot"
+
+	"github.com/Mikhalevich/tg-bonus-points-bot/internal/domain/port/msginfo"
+	"github.com/Mikhalevich/tg-bonus-points-bot/internal/infra/logger"
+)
+
+func (m *messageSender) DeleteMessage(
+	ctx context.Context,
+	chatID msginfo.ChatID,
+	messageID msginfo.MessageID,
+) {
+	if _, err := m.bot.DeleteMessage(ctx, &bot.DeleteMessageParams{
+		ChatID:    chatID.Int64(),
+		MessageID: messageID.Int(),
+	}); err != nil {
+		logger.FromContext(ctx).
+			WithError(err).
+			Error("failed to delete message")
+	}
+}
