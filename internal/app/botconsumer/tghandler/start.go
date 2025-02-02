93584915109e8c2f06f5c59b821e7a3ad687f6b8
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2025-02-02 12:52:00 +0300

    description for menu commands

--- internal/app/botconsumer/tghandler/start.go
+++ internal/app/botconsumer/tghandler/start.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (t *TGHandler) Start(ctx context.Context, msg tgbot.BotMessage, sender tgbot.MessageSender) error {
-	sender.SendMessage(ctx, msg.ChatID, "Type /order for requesting an order")
+	sender.SendMessage(ctx, msg.ChatID, "Tap /order to view and assemble products for you")
 
 	return nil
 }
