c33d7f309b237ff613987cbf32a56dfca44b3511
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2025-03-04 23:40:41 +0300

    run signal interruption func

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/Mikhalevich/tg-bonus-points-bot/internal/config"
 	"github.com/Mikhalevich/tg-bonus-points-bot/internal/infra"
@@ -26,36 +24,32 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := runService(cfg, log); err != nil {
+	if err := tracing.SetupTracer(cfg.Tracing.Endpoint, cfg.Tracing.ServiceName, ""); err != nil {
+		log.WithError(err).Error("failed to setup tracer")
+		os.Exit(1)
+	}
+
+	if err := infra.RunSignalInterruptionFunc(func(ctx context.Context) error {
+		log.Info("starting consumer bot")
+
+		if err := infra.StartBot(
+			ctx,
+			cfg.StoreID,
+			cfg.Bot,
+			cfg.Postgres,
+			cfg.CartRedis,
+			cfg.DailyPositionRedis,
+			cfg.ButtonRedis,
+			log.WithField("bot_name", "consumer"),
+		); err != nil {
+			return fmt.Errorf("start bot: %w", err)
+		}
+
+		log.Info("consumer bot stopped")
+
+		return nil
+	}); err != nil {
 		log.WithError(err).Error("failed run service")
 		os.Exit(1)
 	}
-}
-
-func runService(cfg config.ConsumerBot, log logger.Logger) error {
-	if err := tracing.SetupTracer(cfg.Tracing.Endpoint, cfg.Tracing.ServiceName, ""); err != nil {
-		return fmt.Errorf("setup tracer: %w", err)
-	}
-
-	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer cancel()
-
-	log.Info("starting bot")
-
-	if err := infra.StartBot(
-		ctx,
-		cfg.StoreID,
-		cfg.Bot,
-		cfg.Postgres,
-		cfg.CartRedis,
-		cfg.DailyPositionRedis,
-		cfg.ButtonRedis,
-		log.WithField("bot_name", "schedule"),
-	); err != nil {
-		return fmt.Errorf("start bot: %w", err)
-	}
-
-	log.Info("bot stopped")
-
-	return nil
 }
