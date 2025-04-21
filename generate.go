882f05cf368e8c660cd42ab1374b8c85ce03083f
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2025-04-22 00:52:42 +0300

    go tool mockgen

--- generate.go
+++ generate.go
@@ -1,5 +1,5 @@
 package generate
 
-//go:generate tools/bin/mockgen -source=internal/domain/port/customer_repository.go -destination=internal/domain/port/customer_repository_mock.go -package=port
+//go:generate go tool mockgen -source=internal/domain/port/customer_repository.go -destination=internal/domain/port/customer_repository_mock.go -package=port
 
-//go:generate tools/bin/mockgen -source=internal/domain/port/message_sender.go -destination=internal/domain/port/message_sender_mock.go -package=port
+//go:generate go tool mockgen -source=internal/domain/port/message_sender.go -destination=internal/domain/port/message_sender_mock.go -package=port
