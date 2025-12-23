575dc83f5615762ecb6dff6498d5b2d479797f98
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2025-12-23 23:26:20 +0300

    add columns and filters for django db models

--- cmd/adminpanel/models/admin.py
+++ cmd/adminpanel/models/admin.py
@@ -3,22 +3,43 @@ from .models import Store, Currency, Category, Product, ProductCategory, Product
 
 # Register your models here.
 
-admin.site.register(Store)
-admin.site.register(StoreSchedule)
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ["description", "default_currency"]
+    ordering = ["description"]
+
+@admin.register(StoreSchedule)
+class StoreScheduleAdmin(admin.ModelAdmin):
+    list_display = ["store", "day_of_week", "start_time", "end_time"]
+    ordering = ["store"]
 
 @admin.register(Currency)
 class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ["code", "exp", "decimal_sep", "min_amount", "max_amount", "is_enabled"]
+    search_fields = ["code"]
     ordering = ["code"]
+    list_filter = ["is_enabled"]
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["title", "is_enabled"]
     search_fields = ["title"]
     ordering = ["title"]
+    list_filter = ["is_enabled"]
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "is_enabled", "created_at", "updated_at"]
     search_fields = ["title"]
     ordering = ["title"]
+    list_filter = ["is_enabled", "created_at", "updated_at"]
 
-admin.site.register(ProductCategory)
-admin.site.register(ProductPrice)
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ["category", "product"]
+    ordering = ["category", "product"]
+
+@admin.register(ProductPrice)
+class ProductPriceAdmin(admin.ModelAdmin):
+    list_display = ["product", "currency", "price"]
+    ordering = ["product", "currency"]
